8babaa0c4d11029cb9aded1a17520b5a28db6d6e
pkup1618 <andrey0sintsev@mail.ru>
2026-06-12 10:49:27 +0500

    added files from spring initializer

---
+++ src/main/kotlin/pkup/projects/sbdtfeatures/SbdtFeaturesApplication.kt
@@ -0,0 +1,11 @@
+package pkup.projects.sbdtfeatures
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SbdtFeaturesApplication
+
+fun main(args: Array<String>) {
+	runApplication<SbdtfeaturesApplication>(*args)
+}
