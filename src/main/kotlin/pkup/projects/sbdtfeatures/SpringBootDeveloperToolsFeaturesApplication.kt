5a95cc648095ebe704ddca34226e8d72f19d88d5
pkup1618 <andrey0sintsev@mail.ru>
2026-06-12 11:10:09 +0500

    normalised project after spring initializer

--- src/main/kotlin/pkup/projects/sbdtfeatures/SpringBootDeveloperToolsFeaturesApplication.kt
+++ src/main/kotlin/pkup/projects/sbdtfeatures/SpringBootDeveloperToolsFeaturesApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SbdtFeaturesApplication
+class SpringBootDeveloperToolsFeaturesApplication
 
 fun main(args: Array<String>) {
-	runApplication<SbdtfeaturesApplication>(*args)
+    runApplication<SpringBootDeveloperToolsFeaturesApplication>(*args)
 }
